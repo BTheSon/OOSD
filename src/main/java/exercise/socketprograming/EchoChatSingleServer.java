2067e4a7b1a91e0fd17c33c4b79ed0d84f301752
Bui_The_Son <buitheson201@gmail.com>
2026-01-28 23:05:27 +0700

    add: SingleThread demo

---
+++ src/main/java/exercise/socketprograming/EchoChatSingleServer.java
@@ -0,0 +1,46 @@
+package exercise.socketprograming;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoChatSingleServer {
+
+    public final static int SERVER_PORT = 7;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            System.out.println("Binding to port " + SERVER_PORT + ", please wait  ...");
+            serverSocket = new ServerSocket(SERVER_PORT);
+            System.out.println("Server started: " + serverSocket);
+            System.out.println("Waiting for a client ...");
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Client accepted: " + socket);
+
+                    OutputStream os = socket.getOutputStream();
+                    InputStream is = socket.getInputStream();
+
+                    int ch = 0;
+                    while ((ch = is.read()) != -1) {
+                        os.write(ch);
+                    }
+                    socket.close();
+                } catch (IOException e) {
+                    System.err.println(" Connection Error: " + e);
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+    }
+
+}
